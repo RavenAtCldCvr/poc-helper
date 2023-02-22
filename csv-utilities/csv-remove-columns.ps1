d2591cd47a76835a0b730d764fce68ce91d7d8ad
Raven Ng Shi Jie <shijie.ng@cldcvr.com>
2023-02-23 02:31:00 +0800

    update to bulk exclude properties

--- csv-utilities/csv-remove-columns.ps1
+++ csv-utilities/csv-remove-columns.ps1
@@ -2,23 +2,24 @@
 $csvPath = ""
 
 # Set the names of the columns you want to remove
-$columnsToRemove = @("firstname", "lastname", "b_firstname", "b_lastname", "b_address","b_address_2", "b_city","b_county", "b_state",
-"b_country", "b_zipcode", "b_phone", "s_firstname", "s_lastname", "s_address", "s_address_2", "s_city", "s_county", "s_state", "s_country", "s_zipcode",
-"s_phone", "s_address_type","ip_address","odata_etag", "last_api_log")
+$columnsToRemove = @("lastname", "b_lastname", "b_address","b_address_2", "b_phone", "s_firstname", "s_lastname", "s_address", "s_address_2",
+"s_phone", "s_address_type","ip_address","odata_etag", "last_api_log","payment_surcharge","shipping_ids","shipping_cost",
+"status","notes","details","promotions","promotion_ids","company","phone","fax","url","email","payment_id","tax_exempt","repaid","validation_code","localization_id",
+"profile_id","storefront_id","updated_at","weight","document_no","sd_ga_cid","sd_ga_status","retry_count","retry_after_timestamp","outstanding_amount","bc_status","is_mobile_order")
 
 # Load the CSV file
 $csv = Import-Csv $csvPath
 
 # Loop through each row and column, replacing new lines with whitespace
-foreach ($row in $data) {
-    foreach ($col in $row) {
-        $col = $col -replace "`n", " "
-    }
-}
+#foreach ($row in $data) {
+#    foreach ($col in $row) {
+#        $col = $col -replace "`n", " "
+#    }
+#}
 
-$newCsvPath = "{0}_{1}.csv" -f ($csvPath -replace '\.csv$', ''), 'short'
+$newCsvPath = "{0}_{1}.csv" -f ($csvPath -replace '\.csv$', ''), 'max_with_vendor'
 # Remove the specified columns from the CSV
-$csv = $csv | Select-Object * -ExcludeProperty odata_etag,last_api_log
+$csv = $csv | Select-Object * -ExcludeProperty $columnsToRemove
 
 # Save the modified CSV file
 $csv | Export-Csv $newCsvPath -NoTypeInformation
